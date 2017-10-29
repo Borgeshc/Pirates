11de167ff667caeaa412fdc517a69b846775a283
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-10-28 20:14:22 -0500

    initial commit

---
+++ Assets/Game/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Vector3 offset;
+    public float speed;
+
+    Camera cam;
+    GameObject camPivot;
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        cam = transform.Find("CharacterCamera").GetComponent<Camera>();
+        camPivot = transform.parent.gameObject;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void LateUpdate ()
+    {
+        camPivot.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X")* speed * Time.deltaTime,0));
+        cam.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * speed * Time.deltaTime, 0,0));
+    }
+}
---
+++ Assets/Game/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+    float horizontal;
+    float vertical;
+
+    GameObject cameraPivot;
+    GameObject characterPivot;
+
+    Vector3 movement;
+    Rigidbody rb;
+    Animator anim;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+        cameraPivot = transform.Find("CameraPivot").gameObject;
+        characterPivot = transform.Find("CharacterPivot").gameObject;
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        anim.SetFloat("Horizontal", horizontal);
+        anim.SetFloat("Vertical", vertical);
+
+        movement = new Vector3(horizontal, 0, vertical);
+        movement *= speed * Time.deltaTime;
+        movement = cameraPivot.transform.TransformDirection(movement);
+        movement.y = 0f;
+
+        if(movement != Vector3.zero)
+            characterPivot.transform.rotation = Quaternion.Lerp(characterPivot.transform.rotation, Quaternion.LookRotation(movement), rotationSpeed * Time.deltaTime);
+
+        rb.velocity = movement;
+    }
+}
